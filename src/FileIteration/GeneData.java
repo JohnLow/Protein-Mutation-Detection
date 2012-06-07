b960eff723668b8f786a89d1d68a4e40f27abae5
Immanuel Purushothaman <ipurusho@asu.edu>
2012-06-07 14:14:11 -0700

    initial commit

---
+++ src/FileIteration/GeneData.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FileIteration;
+
+/**
+ *
+ * @author ipurusho
+ */
+public class GeneData {
+ 
+    
+    public String getName(){  //retrieve gene name from TCGA data
+        
+        String geneName = null;
+        
+        
+        
+        return geneName;
+    }
+    
+    
+    
+    public String[] getAccession(){  //retrieve list of accession numbers for isomers 
+        
+        String[] accessionNumber = null;
+    
+    
+        return accessionNumber; 
+    }
+
+    public String getLargest(){
+        
+        String largestIsomer = null;
+        
+        
+        
+        
+        return largestIsomer;
+        
+        
+    }
+
+
+
+
+}
---
+++ src/FileIteration/MutationComparison.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FileIteration;
+
+/**
+ *
+ * @author ipurusho
+ */
+public class MutationComparison {
+        public static void main(String[] args)  {
+   
+        
+        
+        }
+        
+ 
+}
